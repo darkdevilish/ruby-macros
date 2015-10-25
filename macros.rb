61148474fd2cf7cbbf0313182ce044ee9aa8e890
Anthony Gonzalez <darkdevilishag@gmail.com>
2015-10-25 06:21:51 -0400

    Adds class definitions

--- macros.rb
+++ macros.rb
@@ -8,3 +8,31 @@ def dog1.hunt
 end
 
 dog1.hunt
+
+puts "Before class definition"
+
+# Class definitions are executable code
+class Movie
+  puts "Inside class definition of #{self}"
+
+  def self.my_class_method
+    puts "Running class method..."
+  end
+
+  self.my_class_method
+  # Implicitly ruby will use self as the receiver of method call when not specifying self
+  my_class_method
+
+end
+
+puts "After class definition"
+
+puts Movie.class #prints out Class
+puts Movie.object_id
+# It can be outside of class too
+#   def Movie.my_class_method
+#     puts "Running class method..."
+#   end
+
+# In ruby there are no class methods, this is a singleton method define in the class object
+Movie.my_class_method
